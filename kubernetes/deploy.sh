0df8e4fd71f6dd9b8c135928274f0892b572e4df
gruggiero <giovanni.ruggiero@gmail.com>
2018-09-24 19:53:45 +0200

    Reverted to environment variables

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-deployEnv=$1
+# deployEnv=$1
 
-kubectl --kubeconfig=/var/lib/jenkins/.kube/config.teamdigitale-staging delete configmap storage-manager-conf
-kubectl --kubeconfig=/var/lib/jenkins/.kube/config.teamdigitale-staging create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf
-kubectl --kubeconfig=/var/lib/jenkins/.kube/config.teamdigitale-staging replace -f daf-storage-manager-${DEPLOY_ENV}.yml --force
+kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf
+kubectl --kubeconfig=$KUBECONFIG create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf
+kubectl --kubeconfig=$KUBECONFIG replace -f daf-storage-manager-${DEPLOY_ENV}.yml --force
