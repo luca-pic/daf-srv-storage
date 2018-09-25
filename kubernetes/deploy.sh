ccc020344f9a4a7a1c4f4d76653577e53b2eadc7
Cristian Monti <cristianmonti1995@gmail.com>
2018-09-25 10:51:51 +0200

    prova master slave

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf --namespace=testci
+kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf --namespace=testci || true
 kubectl --kubeconfig=$KUBECONFIG create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf --namespace=testci
 kubectl --kubeconfig=$KUBECONFIG replace -f daf-storage-manager-${DEPLOY_ENV}.yml --force --namespace=testci
