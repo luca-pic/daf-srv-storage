71cbbe7eb50ed09d851914ec421daaa41eb4f6da
gruggiero <giovanni.ruggiero@gmail.com>
2018-09-25 10:42:44 +0200

    changed version and namespace

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf
-kubectl --kubeconfig=$KUBECONFIG create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf
-kubectl --kubeconfig=$KUBECONFIG replace -f daf-storage-manager-${DEPLOY_ENV}.yml --force
+kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf --namespace=testci
+kubectl --kubeconfig=$KUBECONFIG create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf --namespace=testci
+kubectl --kubeconfig=$KUBECONFIG replace -f daf-storage-manager-${DEPLOY_ENV}.yml --force --namespace=testci
