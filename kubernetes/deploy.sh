60086826185b42ed51e4bd6fa8b6ee2f06fbb205
gruggiero <giovanni.ruggiero@gmail.com>
2018-12-11 18:08:02 +0100

    Fixed deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -8,5 +8,5 @@ dhall-to-yaml --omitNull <<< ./deployment.dhall >> daf-storage-manager.yml
 
 kubectl --kubeconfig=$KUBECONFIG delete configmap storage-manager-conf || true
 kubectl --kubeconfig=$KUBECONFIG create configmap storage-manager-conf --from-file=../conf/${DEPLOY_ENV}/daf.conf
-kubectl --kubeconfig=$KUBECONFIG delete -f daf-storage-manager.yml --force
-kubectl --kubeconfig=$KUBECONFIG create -f daf-storage-manager.yml --force
+kubectl --kubeconfig=$KUBECONFIG delete daf-storage-manager.yml || true
+kubectl --kubeconfig=$KUBECONFIG create -f daf-storage-manager.yml
